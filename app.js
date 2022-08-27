dd6683af4ef96d6567a3e19ce4f4f6dfa854d822
Jorge Rivera <jrivera@VESSELARG.local>
2022-08-27 08:41:05 -0300

    modification of dom ready, next validation or style or submit

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("App working")
+
+
+
+
+
+
+
+function showCardInfo (id) {
+
+    let userValue = document.getElementById(id).value
+    document.getElementById(`${id}Info`).innerHTML = userValue.toUpperCase();
+
+    console.log("hello")
+}
+
+
+
